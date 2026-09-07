036ef8bef79b17629230a0c2e6521eef2eb09222
Thomas100000 <thomasjob500@gmail.com>
2026-09-07 15:45:12 +0530

    Delete sort_dataframe.py

--- sort_dataframe.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df=pd.DataFrame({'Name':[3,1,2],
-'Mark':[89,88,87],
-'Course':['BCA','MCA','IMCA']})
-print(df.to_string())
-print('Sorted values:')
-order=df.sort_values(by=['Name','Mark'],ascending=[True,True])
-print(order.to_string())
