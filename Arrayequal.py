8d8f160e98c2831029c9ea3e70b6367603140f2e
Thomas100000 <thomasjob500@gmail.com>
2026-09-07 15:24:30 +0530

    Delete Arrayequal.py

--- Arrayequal.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-arr1=np.array([1,2,3,4,5])
-arr2=np.array([1,2,3,4,5])
-print(arr1)
-print(arr2)
-
-print("\narrays are equal or not")
-print(arr1 == arr2)
-print(np.equal(arr1, arr2))
