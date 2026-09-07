efdfb5b1dc559c2029ca4fd8a685d863dbc79417
Thomas100000 <thomasjob500@gmail.com>
2026-09-07 15:47:15 +0530

    Delete Nanvalues.py

--- Nanvalues.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-occ={'Set_of_occupations':['A',np.nan,'B',np.nan,'D','E']}
-df=pd.DataFrame(occ,columns=['Set_of_occupations'])
-df['Set_of_occupations']=df['Set_of_occupations'].fillna(0)
-print(df)
